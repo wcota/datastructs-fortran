3d737023c07ca57e2f2fca88ab48a996865bdac3
Wesley Cota <wlcota@gmail.com>
2025-04-06 19:37:30 -0300

    add qp precision

--- src/kinds.f90
+++ src/kinds.f90
@@ -2,6 +2,7 @@ module hOGA_kinds_mod
     use, intrinsic :: iso_fortran_env, only: &
         sp => real32, & ! single precision, range: -3.40282347E+38 to 3.40282347E+38, smallest positive: 1.17549435E-38
         dp => real64, & ! double precision, range: -1.7976931348623157E+308 to 1.7976931348623157E+308, smallest positive: 2.2250738585072014E-308
+        qp => real128, &
         i1 => int8, &   ! 1 byte integer, range: -128 to 127
         i2 => int16, &  ! 2 byte integer, range: -32768 to 32767
         i4 => int32, &  ! 4 byte integer, range: -2147483648 to 2147483647
@@ -18,6 +19,7 @@ contains
     subroutine list_ranges()
         real(kind=sp) :: real_sp
         real(kind=dp) :: real_dp
+        real(kind=qp) :: real_qp
         integer(kind=i1) :: int_i1
         integer(kind=i2) :: int_i2
         integer(kind=i4) :: int_i4
@@ -26,6 +28,7 @@ contains
         ! Print the ranges of the types
         write(*,*) "Single precision range (positive): ", tiny(real_sp), " to ", huge(real_sp)
         write(*,*) "Double precision range (positive): ", tiny(real_dp), " to ", huge(real_dp)
+        write(*,*) "Quadruple precision range (positive): ", tiny(real_qp), " to ", huge(real_qp)
         write(*, *) "1 byte integer range: ", -huge(int_i1) - 1, " to ", huge(int_i1)
         write(*, *) "2 byte integer range: ", -huge(int_i2) - 1, " to ", huge(int_i2)
         write(*, *) "4 byte integer range: ", -huge(int_i4) - 1, " to ", huge(int_i4)
