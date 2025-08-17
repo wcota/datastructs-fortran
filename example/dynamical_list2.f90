59ba945d65c71cb41cfdca6762d44b5db53490b9
Wesley Cota <wlcota@gmail.com>
2025-08-17 20:47:24 -0300

    feat: add test programs for dynamical list and hash functionalities

---
+++ example/dynamical_list2.f90
@@ -0,0 +1,49 @@
+program test_dynamical_list2
+    use kinds_mod
+    use datastructs_fortran, only: dynamical_list_t
+    use rndgen_mod
+    implicit none
+
+    type(dynamical_list_t) :: list
+    type(rndgen) :: rnd 
+
+    call rnd%init(12345)
+
+    ! Initilize the list of size 50 with random integers
+    list = rnd%rnd_array(50, 1, 100)
+    call list%print()
+
+    call list%remove(1)
+    call list%print()
+
+    call list%add(10)
+    call list%print()
+
+    call list%remove(2)
+    call list%print()
+
+    call list%remove(3)
+    call list%print()
+
+    call list%add([1,2])
+    call list%print()
+
+    call list%remove([20,21])
+    call list%print()
+
+    call list%remove(20,30)
+    call list%print()
+
+    call list%reset()
+    call list%print()
+
+    call list%add(rnd%rnd_array(10, 1, 100))
+    call list%print()
+
+    call list%trim()
+    call list%print()
+    write(*,fmt_general) 'Sum = ', list%sum()
+
+    list = [1,2,3] ! will trigger error
+
+end program test_dynamical_list2
---
+++ example/hash.f90
@@ -0,0 +1,36 @@
+program test_hash
+    use kinds_mod
+    use datastructs_fortran, only : djb2
+    use rndgen_mod
+    use stdlib_sorting, only : sort, sort_index
+    implicit none
+
+    type(rndgen) :: gen
+
+    integer(kind=i4), allocatable :: list(:)
+    integer(kind=i4), allocatable :: list_index(:)
+
+    call gen%init(1213213)
+
+    write(*,*) 'Generated list of random integers:'
+    list = gen%rnd_array(10, 1, 1000000000)
+    
+    write(*, fmt_general) list
+    write(*, fmt_general) 'Calculating hash:'
+    write(*,fmt_general) 'Hash value of the list:', djb2(list)
+
+    allocate(list_index(size(list)))
+    write(*,fmt_general) 'Sorting the list using sort_index():'
+    call sort_index(list, list_index)
+    write(*, fmt_general) list
+    write(*, fmt_general) list_index
+    write(*, fmt_general) 'Calculating hash:'
+    write(*,fmt_general) 'Hash value of the list:', djb2(list)
+
+    write(*,fmt_general) 'Sorting the list using sort():'
+    call sort(list)
+    write(*, fmt_general) list
+    write(*, fmt_general) 'Calculating hash:'
+    write(*,fmt_general) 'Hash value of the list:', djb2(list)
+
+end program test_hash
