2143063eb27965a22f1dc76d01ab5140b8e5aa76
Wesley Cota <wlcota@gmail.com>
2026-02-10 16:55:08 -0300

    update docs

--- docs/src/fixed_list.f90
+++ docs/src/fixed_list.f90
@@ -10,11 +10,11 @@
 !>     type(fixed_list_t), target :: my_list2
 !>     my_list = fixed_list([1,2,3,4,5])
 !>     my_list2 = fixed_list([6,7,8])
-!> 
+!>
 !>     call my_list%print()
-!> 
+!>
 !>     my_list%next => my_list2
-!> 
+!>
 !>     call my_list%next%print()
 !> end program example_fixed_list
 !> ```
